3da23a5c8bade358920c220402d46a15775acb54
ShadowGreg <shadow-greg@yandex.ru>
2023-08-01 16:24:23 +0300

    начал написание и тестирование заполнение щита

--- ElectricalEngineeringLiteV1/BackendTests/ElectricalPanelFillControllerTests.cs
+++ ElectricalEngineeringLiteV1/BackendTests/ElectricalPanelFillControllerTests.cs
@@ -1,5 +1,31 @@
-﻿namespace BackendTests {
+﻿using System.Collections.Generic;
+using BillingFillingController.Contrlollers.ElectricalPanel;
+using CoreV01.Feeder;
+using NUnit.Framework;
+
+namespace BackendTests {
+    [TestFixture]
     public class ElectricalPanelFillControllerTests {
-        
+        private List<BaseConsumer> _consumers;
+        private ElectricalPanelFillController _electricalPanelFillController;
+
+        [SetUp]
+        public void Setup() {
+            _electricalPanelFillController = new ElectricalPanelFillController();
+        }
+
+        [Test]
+        public void ElectricalPanelFillController_Test() {
+            // Arrange
+            _electricalPanelFillController.AddOnPanel(new List<BaseConsumer> {
+                new BaseConsumer()
+            });
+
+            // Act
+            var actualPanel = _electricalPanelFillController.GetPanel();
+
+            // Assert
+            Assert.NotNull(actualPanel);
+        }
     }
 }
--- ElectricalEngineeringLiteV1/CoreV01/BaseElectricalPanel.cs
+++ ElectricalEngineeringLiteV1/CoreV01/BaseElectricalPanel.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Data;
 using CoreV01.Feeder;
 
 namespace CoreV01.Properties {
